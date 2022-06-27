da54c61abee35924901bdf36ab6b244fad24f8c1
nevadskiy <nevadskiy@gmail.com>
2022-06-27 21:27:31 +0300

    Add curl downloader class

---
+++ src/CurlDownloader.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nevadskiy\Downloader;
+
+class CurlDownloader implements Downloader
+{
+    /**
+     * @inheritdoc
+     */
+    public function download(string $url, string $directory, string $name = null)
+    {
+        //
+    }
+}

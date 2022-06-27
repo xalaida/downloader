79cbacc0fad14718ea936fc19565b8dfc540bc36
nevadskiy <nevadskiy@gmail.com>
2022-06-27 21:53:39 +0300

    Add base curl download logic

--- src/CurlDownloader.php
+++ src/CurlDownloader.php
@@ -2,6 +2,8 @@
 
 namespace Nevadskiy\Downloader;
 
+use Exception;
+
 class CurlDownloader implements Downloader
 {
     /**
@@ -9,6 +11,65 @@ class CurlDownloader implements Downloader
      */
     public function download(string $url, string $directory, string $name = null)
     {
-        //
+        $name = $name ?: $this->guessFileName($url);
+
+        $path = $this->getFilePath($directory, $name);
+
+        $stream = fopen($path, 'wb+');
+
+        if ($stream === false) {
+            throw new Exception('Could not open: ' . $path);
+        }
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_FILE, $stream);
+
+        // Timeout if the file doesn't download after 20 seconds.
+        // curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+
+        // Execute the request.
+        curl_exec($ch);
+
+        // If there was an error, throw an Exception
+        if (curl_errno($ch)) {
+            throw new Exception(curl_error($ch));
+        }
+
+        // Get the HTTP status code.
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // Close the cURL handler.
+        curl_close($ch);
+
+        // Close the file handler.
+        fclose($stream);
+
+        if ($statusCode === 200) {
+            echo 'Downloaded!';
+        } else {
+            echo "Status Code: " . $statusCode;
+        }
+    }
+
+    private function guessFileName(string $url): string
+    {
+        $position = strrpos($url, '/');
+
+        if ($position === false) {
+            // cannot define file name...
+        }
+
+        return substr($url, $position + 1);
+    }
+
+    /**
+     * @param string $directory
+     * @param string $name
+     * @return string
+     */
+    protected function getFilePath(string $directory, string $name): string
+    {
+        return rtrim($directory, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . trim($name, DIRECTORY_SEPARATOR);
     }
 }
--- src/Downloader.php
+++ src/Downloader.php
@@ -6,6 +6,8 @@ interface Downloader
 {
     /**
      * Download a file by the URL to the given directory.
+     *
+     * @return void
      */
     public function download(string $url, string $directory, string $name = null);
 }

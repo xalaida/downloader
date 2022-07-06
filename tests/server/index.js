490c0e4452e29416f2d63e13dfd2adcaa9991ef3
nevadskiy <nevadskiy@gmail.com>
2022-07-06 23:59:52 +0300

    Migrate to JS server

---
+++ tests/server/index.js
@@ -0,0 +1,25 @@
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+    if (req.url === '/') {
+        res.write('Welcome home!');
+        res.end();
+    } else if (req.url.startsWith('/redirect')) {
+        res.writeHead(301, { 'Location': req.url.replace('/redirect', '/fixtures') });
+        res.end();
+    } else if (req.url.startsWith('/fixtures')) {
+        fs.readFile(__dirname + req.url, function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+}).listen(8888);
